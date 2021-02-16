3713cbd74f831367555a990a258db0abcae056b4
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-16 20:24:26 +0000

    Speed things up a bit.

--- scripts/run_monocular_skeleton_detection_service.py
+++ scripts/run_monocular_skeleton_detection_service.py
@@ -1,6 +1,7 @@
-import cv2
 import numpy as np
+import time
 
+from timeit import default_timer as timer
 from typing import List
 
 from smg.comms.skeletons import SkeletonDetectionService
@@ -10,21 +11,27 @@ from smg.skeletons import Skeleton
 
 def frame_processor(skeleton_detector: SkeletonDetector):
     def inner(colour_image: np.ndarray, _, world_from_camera: np.ndarray) -> List[Skeleton]:
-        # cv2.imshow("Colour Image", colour_image)
-        # cv2.waitKey()
-        # cv2.destroyAllWindows()
+        start = timer()
         skeletons, _ = skeleton_detector.detect_skeletons(colour_image, world_from_camera)
+        end = timer()
+        print(f"Detection Time: {end - start}s")
         return skeletons
     return inner
 
 
 def main() -> None:
-    skeleton_detector: SkeletonDetector = SkeletonDetector()
+    skeleton_detector: SkeletonDetector = SkeletonDetector(debug=True)
+
+    # import cv2
+    # image: np.ndarray = cv2.imread("D:/LCRNet_v2.0/skeleton.png")
+    # world_from_camera: np.ndarray = np.eye(4)
+    # while True:
+    #     skeletons = frame_processor(skeleton_detector)(image, image, world_from_camera)
 
     with SkeletonDetectionService(frame_processor=frame_processor(skeleton_detector)) as service:
         service.start()
         while not service.should_terminate():
-            pass
+            time.sleep(0.1)
 
 
 if __name__ == "__main__":
