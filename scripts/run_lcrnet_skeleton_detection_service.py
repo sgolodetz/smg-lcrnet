13a419dfcdd422065106d0318622262624532471
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-02-17 15:55:01 +0000

    Minor renaming.

--- scripts/run_lcrnet_skeleton_detection_service.py
+++ scripts/run_lcrnet_skeleton_detection_service.py
@@ -9,13 +9,13 @@ from smg.skeletons import Skeleton
 
 
 def make_frame_processor(skeleton_detector: SkeletonDetector):
-    def process_frame(colour_image: np.ndarray, _, world_from_camera: np.ndarray) -> List[Skeleton]:
+    def detect_skeletons(colour_image: np.ndarray, _, world_from_camera: np.ndarray) -> List[Skeleton]:
         start = timer()
         skeletons, _ = skeleton_detector.detect_skeletons(colour_image, world_from_camera)
         end = timer()
         print(f"Detection Time: {end - start}s")
         return skeletons
-    return process_frame
+    return detect_skeletons
 
 
 def main() -> None:
