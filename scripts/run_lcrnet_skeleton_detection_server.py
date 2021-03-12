9132d88ea6ac8a3ecb19c680fb1a3ebf10cb6f96
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-03-12 12:33:52 +0000

    Revert some unwanted changes.

--- scripts/run_lcrnet_skeleton_detection_server.py
+++ scripts/run_lcrnet_skeleton_detection_server.py
@@ -20,7 +20,7 @@ from smg.opengl import OpenGLMatrixContext, OpenGLUtil
 from smg.rigging.cameras import SimpleCamera
 from smg.rigging.controllers import KeyboardCameraController
 from smg.rigging.helpers import CameraPoseConverter
-from smg.skeletons import Skeleton, SkeletonRenderer, SkeletonUtil
+from smg.skeletons import Skeleton, SkeletonRenderer
 from smg.utility import GeometryUtil, PooledQueue
 
 
@@ -51,7 +51,6 @@ def main() -> None:
         image_size: Optional[Tuple[int, int]] = None
         intrinsics: Optional[Tuple[float, float, float, float]] = None
         receiver: RGBDFrameReceiver = RGBDFrameReceiver()
-        render_w_t_c: np.ndarray = np.eye(4)
         skeletons_3d: List[Skeleton] = []
 
         # Construct the skeleton detector.
@@ -83,9 +82,7 @@ def main() -> None:
                 # Get the newest frame from the server.
                 server.peek_newest_frame(client_id, receiver)
                 colour_image: np.ndarray = receiver.get_rgb_image()
-                depth_image: np.ndarray = receiver.get_depth_image()
                 tracker_w_t_c: np.ndarray = receiver.get_pose()
-                render_w_t_c = tracker_w_t_c
 
                 # Use LCR-Net to detect 3D skeletons in the colour image.
                 start = timer()
@@ -114,39 +111,18 @@ def main() -> None:
                 )):
                     # Set the model-view matrix.
                     with OpenGLMatrixContext(GL_MODELVIEW, lambda: OpenGLUtil.load_matrix(
-                        CameraPoseConverter.pose_to_modelview(np.linalg.inv(render_w_t_c))
+                        CameraPoseConverter.pose_to_modelview(camera_controller.get_pose())
                     )):
-                        # # Render a voxel grid.
-                        # glColor3f(0.0, 0.0, 0.0)
-                        # OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
+                        # Render a voxel grid.
+                        glColor3f(0.0, 0.0, 0.0)
+                        OpenGLUtil.render_voxel_grid([-2, -2, -2], [2, 0, 2], [1, 1, 1], dotted=True)
 
                         # Render the 3D skeletons.
                         for skeleton_3d in skeletons_3d:
-                            # SkeletonRenderer.render_skeleton(skeleton_3d)
-                            glColor3f(0, 0, 0)
-                            SkeletonRenderer.render_bounding_shapes(skeleton_3d)
+                            SkeletonRenderer.render_skeleton(skeleton_3d)
 
             # Swap the front and back buffers.
             pygame.display.flip()
-
-            if image_size is not None:
-                # start = timer()
-                buffer = glReadPixels(0, 0, *window_size, GL_BGR, GL_UNSIGNED_BYTE)
-                mask = np.frombuffer(buffer, dtype=np.uint8).reshape((480, 640, 3))[::-1, :]
-                # end = timer()
-                # print(f"Read Time: {end - start}s")
-
-                if len(skeletons_3d) == 1:
-                    ws_points: np.ndarray = GeometryUtil.compute_world_points_image_fast(
-                        depth_image, tracker_w_t_c, intrinsics
-                    )
-
-                    person_mask = SkeletonUtil.make_person_mask(skeletons_3d[0], depth_image, ws_points)
-                    mask = np.where((mask[:, :, 0] == 0) & (person_mask != 0), 255, 0).astype(np.uint8)
-
-                depth_image_uc: np.ndarray = np.clip(depth_image * 255 / 5, 0, 255).astype(np.uint8)
-                cv2.imshow("Mask", (np.atleast_3d(depth_image_uc) * 0.5 + np.atleast_3d(mask) * 0.5).astype(np.uint8))
-                cv2.waitKey(1)
 
 
 if __name__ == "__main__":
